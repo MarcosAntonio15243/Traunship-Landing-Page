6d72f3617b2e31173f68c1772d1dbf18c06e7934
MarcosAntonio15243 <marcosantonio15243@gmail.com>
2025-04-23 20:59:50 -0300

    feat: Adicionando tags básicas do header

---
+++ src/components/layout/Header.tsx
@@ -0,0 +1,16 @@
+
+
+export default function Header() {
+  return (
+    <header>
+      <span>Traunship</span>
+      <nav>
+        <a href="#">Features</a>
+        <a href="#">Products</a>
+        <a href="#">Testemonial</a>
+        <a href="#">Contact</a>
+      </nav>
+      <button>Try a demo</button>
+    </header>
+  )
+}
