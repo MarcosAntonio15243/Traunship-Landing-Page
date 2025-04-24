7ebb11a7320447cd53095135f02aaea26793a49e
MarcosAntonio15243 <marcosantonio15243@gmail.com>
2025-04-23 22:35:37 -0300

    feat: Adicionando texto base da seção principal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,23 @@ import Header from "@/components/layout/Header";
 
 export default function Home() {
   return (
-    <div className="">
-      <Header />
-    </div>
+    <main className="">
+      <section className="relative px-40 py-10 min-h-screen bg-gradient-to-b bg-[url('/assets/rocket-launch.webp')] bg-bottom bg-cover">
+
+        {/* Overlay */}
+        <div className="absolute inset-0 bg-black/30 z-0" />
+
+        <div className="relative z-10 flex flex-col items-center justify-center h-full text-white">
+          <Header />
+          <div className="w-full flex flex-col justify-center items-center gap-6">
+            <h2 className="text-white text-6xl font-bold mt-32 text-shadow-[4px_2px_4px_rgb(0_0_0_/_0.25)]">Best place to buy rocket</h2>
+            <p>Quis ipsum pellentesque nulla nulla elementum sagittis dictum</p>
+          </div>
+        </div>
+
+      </section>
+
+      <p>sdsd</p>
+    </main>
   );
 }
--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -3,9 +3,9 @@ export default function Header() {
   let navItens = ["Features", "Products", "Testemonial", "Contact"];
 
   return (
-    <header className="w-full flex flex-row justify-between items-center">
-      <span className="font-bold cursor-pointer text-xl">Traunship</span>
-      <nav className={`flex flex-row gap-10`}>
+    <header className="w-full flex flex-row justify-between items-center text-[var(--white)]">
+      <h1 className="font-bold cursor-pointer text-2xl">Traunship</h1>
+      <nav className={`flex flex-row gap-10 text-sm`}>
         {
           navItens.map((e, index) => {
             return (
@@ -14,7 +14,7 @@ export default function Header() {
           })
         }
       </nav>
-      <button className="cursor-pointer text-sm text-[var(--white)] bg-[var(--caramel)] px-5 py-2 rounded-full">Try a demo</button>
+      <button className="cursor-pointer text-sm bg-[var(--caramel)] px-5 py-2 rounded-full">Try a demo</button>
     </header>
   )
 }
