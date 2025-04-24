c5833bf61471d1d6dfde92c5b059faa8af31dc04
MarcosAntonio15243 <marcosantonio15243@gmail.com>
2025-04-23 23:20:12 -0300

    fix: Adicionando comentários a seção principal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,17 @@ import Header from "@/components/layout/Header";
 export default function Home() {
   return (
     <main className="">
+      {/* Main Section */}
       <section className="relative px-40 py-10 min-h-[110vh] bg-[url('/assets/background.webp')] bg-bottom bg-cover">
         <Header />
+        {/* Content */}
         <div className="w-full flex flex-col justify-center items-center gap-6 mt-32 text-white">
+          {/* Play Button */}
           <div className="bg-[var(--caramel)] p-5 rounded-full mb-5"><img src="/assets/play.svg" alt="Vector" /></div>
+          {/* Text Content */}
           <h2 className="text-white text-6xl font-bold text-shadow-[4px_2px_4px_rgb(0_0_0_/_0.25)]">Best place to buy rocket</h2>
           <p>Quis ipsum pellentesque nulla nulla elementum sagittis dictum</p>
+          {/* Actions Buttons */}
           <div className="flex gap-5">
             <ActionButton>Try a demo</ActionButton>
             <ActionButton type="secondary">Buy now</ActionButton>
