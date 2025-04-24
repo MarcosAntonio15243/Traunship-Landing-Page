9c74541e4549a8c361e2dbd4a1f73af5f1b3e768
MarcosAntonio15243 <marcosantonio15243@gmail.com>
2025-04-23 21:24:59 -0300

    style: Estilizando itens do header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,16 +1,20 @@
 
-
 export default function Header() {
+  let navItens = ["Features", "Products", "Testemonial", "Contact"];
+
   return (
-    <header>
-      <span>Traunship</span>
-      <nav>
-        <a href="#">Features</a>
-        <a href="#">Products</a>
-        <a href="#">Testemonial</a>
-        <a href="#">Contact</a>
+    <header className="w-full flex flex-row justify-between items-center">
+      <span className="font-bold cursor-pointer text-xl">Traunship</span>
+      <nav className={`flex flex-row gap-10`}>
+        {
+          navItens.map((e, index) => {
+            return (
+              <a key={index} href="#" className="hover:underline">{e}</a>
+            )
+          })
+        }
       </nav>
-      <button>Try a demo</button>
+      <button className="cursor-pointer text-sm text-[var(--white)] bg-[var(--caramel)] px-5 py-2 rounded-full">Try a demo</button>
     </header>
   )
 }
