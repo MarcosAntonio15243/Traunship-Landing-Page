e73cd2aa499ae5aed221be31b46eaaaa286fb258
MarcosAntonio15243 <marcosantonio15243@gmail.com>
2025-04-25 00:34:17 -0300

    fix: Corrigindo estilização e centralização de componentes e textos da seção principal de apresentação

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,13 +8,12 @@ export default function Home() {
   return (
     <main className="">
       {/* Main Section */}
-      {/* <section className="relative px-40 py-10 min-h-[110vh] bg-[url('/assets/background.webp')] bg-bottom bg-cover"> */}
       <Section styles="relative min-h-[110vh] bg-[url('/assets/background.webp')] bg-bottom bg-cover">
         <Header />
         {/* Content */}
-        <div className="w-full flex flex-col justify-center items-center gap-6 mt-32 text-white">
+        <div className="w-full flex flex-col justify-center items-center text-center gap-6 mt-32 text-white">
           {/* Play Button */}
-          <div className="bg-[var(--caramel)] p-5 rounded-full mb-5"><img src="/assets/play.svg" alt="Vector" /></div>
+          <div className="bg-[var(--caramel)] cursor-pointer p-5 rounded-full mb-5"><img src="/assets/play.svg" alt="Vector" /></div>
           {/* Text Content */}
           <h2 className="text-white text-6xl font-bold text-shadow-[4px_2px_4px_rgb(0_0_0_/_0.25)]">Best place to buy rocket</h2>
           <p>Quis ipsum pellentesque nulla nulla elementum sagittis dictum</p>
@@ -25,7 +24,6 @@ export default function Home() {
           </div>
         </div>
       </Section>
-      {/* </section> */}
 
       <section className="px-40 py-28 flex flex-col items-center gap-10 text-center">
         <h2 className="text-6xl font-bold">Features you can's avoid</h2>
